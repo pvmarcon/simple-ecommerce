d9b44a584d996b93735041d2b2390788533abff3
pvmarcon <kuromai73@gmail.com>
2026-09-05 22:18:52 -0300

    Database seeding

--- src/main/java/com/pvmarcon/dscommerce/entities/Product.java
+++ src/main/java/com/pvmarcon/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.pvmarcon.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -80,5 +81,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrder(){
+        return items.stream().map(OrderItem::getOrder).toList();
     }
 }
